971bb12eb03b3da166f3dee9f52b5b37effbea29
ELbee <85691313+woodyfi@users.noreply.github.com>
2025-02-12 01:25:08 +0700

    AndroidProducts.mk
    Signed-off-by: ELbee <85691313+woodyfi@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_marble.mk
+    $(LOCAL_DIR)/pixelage_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_marble-user \
-    lineage_marble-userdebug \
-    lineage_marble-eng
+    pixelage_marble-user \
+    pixelage_marble-userdebug \
+    pixelage_marble-eng
